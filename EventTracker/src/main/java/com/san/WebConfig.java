21a01c2c9cbcfa86aa8387f080b74a97e4cbdc34
sanishmr <sanishmr@gmail.com>
2019-04-15 16:56:22 -0400

    Initial Commit

---
+++ EventTracker/src/main/java/com/san/WebConfig.java
@@ -0,0 +1,12 @@
+package com.san;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration 
+@EnableWebMvc
+@ComponentScan(basePackages="com.san")
+public class WebConfig {
+
+}
